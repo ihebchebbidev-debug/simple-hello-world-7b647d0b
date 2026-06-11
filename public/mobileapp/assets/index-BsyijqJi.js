82886ddaa357a18105db6139b239b137cf6982c5
ihebchebbidev-debug <ihebchebbidev@gmail.com>
2026-06-11 20:48:16 +0100

    Rebuild mobileapp bundle with latest changes

---
+++ public/mobileapp/assets/index-BsyijqJi.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CqAH1vV4.js","assets/index-BIMMvON_.js","assets/index-Bemuz45R.css"])))=>i.map(i=>d[i]);
+import{a1 as p,_ as r}from"./index-BIMMvON_.js";const _=p("App",{web:()=>r(()=>import("./web-CqAH1vV4.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
