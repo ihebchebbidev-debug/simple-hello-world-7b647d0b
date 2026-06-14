9f560b0cf784a230dd133a4d5f8b727a4eab6a41
ihebchebbi1998tn <iheb@spadadibattaglia.com>
2026-06-14 17:27:56 +0100

    ad

--- mobile_app/capacitor.config.ts
+++ mobile_app/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'tn.agrisync.app',
-  appName: 'Agri-Sync',
+  appId: 'com.flehty.app',
+  appName: 'Flehty',
   webDir: 'dist',
   bundledWebRuntime: false,
   server: {
