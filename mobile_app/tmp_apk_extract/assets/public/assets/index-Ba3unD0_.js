3383835241e03dfc89c921c1b8ad5fd436da1c2e
ihebchebbi1998tn <iheb@spadadibattaglia.com>
2026-06-17 20:01:48 +0100

    ad

---
+++ mobile_app/tmp_apk_extract/assets/public/assets/index-Ba3unD0_.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DlpxWy7n.js","assets/index-CMQtgQIW.js","assets/index-z6J9vmcl.css"])))=>i.map(i=>d[i]);
+import{a1 as r,_ as t}from"./index-CMQtgQIW.js";const a=r("SplashScreen",{web:()=>t(()=>import("./web-DlpxWy7n.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{a as SplashScreen};
